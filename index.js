fe143eb032ba7161f4967f4b3e848ba8f48d31ca
Aare <aare212@hotmail.com>
2020-11-20 15:40:26 +0200

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${koko} people</p> ${nyt}`)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
